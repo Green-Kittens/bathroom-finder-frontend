ccadb3a8214b3f2347eabaecefcc6a1de186d1a1
unknown <kaylatran2021@gmail.com>
2024-03-03 15:19:39 -0800

    feat: more user func

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -5,7 +5,7 @@
  * @param {string} username - The username of the user *encrypted
  * @param {string} email - The email address of the user
  * @param {string} password - The password of the user *encrypted
- * @param {Image} profile = Optional profile display image
+ * @param {Image} profile - Optional profile display image
  * @returns {Promise<string>} - Returns a promise with a success message upon successful registration
  */
 async function registerUser(
@@ -17,6 +17,7 @@ async function registerUser(
   profile: typeof Image
 ): Promise<string> {
   // Implementation goes here
+  //must create ID for user
 }
 
 /**
@@ -31,11 +32,62 @@ async function loginUser(user: string, password: string): Promise<string> {
 
 /**
  * Function for forgot password
- * @param {string} email = The email of the user
+ * @param {string} email - The email of the user
+ * @returns {Promise<string>} - Returns a promise with a success message upon successfully emailing reset password link
  */
 async function forgotPassword(email: string): Promise<string> {
   //Implementation goes here
 }
 
+/**
+ * Function for retrieving user profile information
+ * @param {string} userID - The ID of the user
+ * @returns {Promise<UserProfile>} - Returns a promise with user profile information
+ */
+async function getUserProfile(email: string): Promise<UserProfile> {
+  //Implementation goes here
+}
+
+/**
+ * Function for creating a bathroom
+ * @param {string} userID - The ID of the user
+ * @param {string} opHours - The hours of operation of the bathroom (days and times)
+ * @param {string} description - *Optional description of the bathroom
+ * @param {Image} images - *Optional images of the bathroom
+ * @returns {Promise<string>} - Returns a promise with a success message upon successfully creating a new bathroom
+ */
+async function createBathroom(
+  userID: string,
+  opHours: string,
+  description: string,
+  images: typeof Image
+): Promise<string> {
+  //Implementation goes here
+}
+
+/**
+ * Function for creating a review
+ * @param {string} userID - The ID of the user
+ * @param {int} rating - The user's rating of the bathroom
+ * @param {Date} time - The date and time the review was uploaded
+ * @param {Image} images - *Optional images
+ * @returns {Promise<string>} - Returns a promise with a success message upon successfully creating a new review
+ */
+async function createReview(
+  userID: string,
+  rating: Int16Array,
+  time: Date,
+  images: typeof Image
+): Promise<string> {
+  //Implementation goes here
+}
+
 // Exporting functions
-export { registerUser, loginUser };
+export {
+  registerUser,
+  loginUser,
+  forgotPassword,
+  getUserProfile,
+  createBathroom,
+  createReview,
+};
