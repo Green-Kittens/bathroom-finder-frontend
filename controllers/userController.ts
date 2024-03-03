dbf49470efc698712c650afaed016cccf03a2563
unknown <kaylatran2021@gmail.com>
2024-03-03 14:49:16 -0800

    feat: controller file structure and func signatures for users

---
+++ controllers/userController.ts
@@ -0,0 +1,33 @@
+/**
+ * Function to register a new user
+ * @param {string} firstname - The first name of the user
+ * @param {string} lastname - The last name of the user
+ * @param {string} username - The username of the user *encrypted
+ * @param {string} email - The email address of the user
+ * @param {string} password - The password of the user *encrypted
+ * @param {Image} profile = Optional profile display image
+ * @returns {Promise<string>} - Returns a promise with a success message upon successful registration
+ */
+async function registerUser(
+  firstname: string,
+  lastname: string,
+  username: string,
+  email: string,
+  password: string,
+  profile: typeof Image
+): Promise<string> {
+  // Implementation goes here
+}
+
+/**
+ * Function to log in a user
+ * @param {string} user - The username or email of the user
+ * @param {string} password - The password of the user
+ * @returns {Promise<string>} - Returns a promise with a success message upon successful login
+ */
+async function loginUser(user: string, password: string): Promise<string> {
+  // Implementation goes here
+}
+
+// Exporting functions
+export { registerUser, loginUser };
