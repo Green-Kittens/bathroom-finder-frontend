12caa185216323b60078dc87fd3f9591f512f8e4
lindlands <lindlands@outlook.com>
2024-03-04 15:41:13 -0800

    margin fixes

--- navigation/screens/facility-reviews.tsx
+++ navigation/screens/facility-reviews.tsx
@@ -9,25 +9,27 @@ export default function FacilityReviewsScreen() {
   return (
     <ScrollView>
       <View style={styles.container}>
-        <ImageBackground
-          source={blobimage}
-          style={{
-            width: 953,
-            height: 1069,
-            position: "absolute",
-            top: 0,
-            left: -200,
-          }}
-          imageStyle={{
-            resizeMode: "cover",
-            alignSelf: "flex-end",
-          }}
-        ></ImageBackground>
-        {Review()}
-        {Review()}
-        {Review()}
-        {Review()}
-        {Review()}
+        <View style={styles.subcontainer}>
+          <ImageBackground
+            source={blobimage}
+            style={{
+              width: 953,
+              height: 1069,
+              position: "absolute",
+              top: 0,
+              left: -200,
+            }}
+            imageStyle={{
+              resizeMode: "cover",
+              alignSelf: "flex-end",
+            }}
+          ></ImageBackground>
+          {Review()}
+          {Review()}
+          {Review()}
+          {Review()}
+          {Review()}
+        </View>
       </View>
     </ScrollView>
   );
@@ -40,5 +42,14 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "#EEF8F7",
+  },
+  subcontainer: {
+    flexDirection: "column",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#EEF8F7",
+    width: "90%",
+    marginVertical: 15,
   },
 });
