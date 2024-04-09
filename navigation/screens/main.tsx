f18e69ea610101f2b57c8deae8bcb62190a8767f
Dylan Tompkins <dltompki@outlook.com>
2024-04-08 20:13:56 -0700

    feat: add loading circle while location is being obtained

--- navigation/screens/main.tsx
+++ navigation/screens/main.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text } from "react-native";
+import { ActivityIndicator, StyleSheet, View } from "react-native";
 import MapView from "react-native-maps";
 
 import * as Location from "expo-location";
@@ -17,10 +17,20 @@ export default function MainScreen() {
         setLocation(await Location.getCurrentPositionAsync());
       }
     })();
-  }, [location]);
+  }, []);
 
   if (location === undefined) {
-    return <Text>Location acquisition in progress...</Text>;
+    return (
+      <View
+        style={{
+          flex: 1,
+          flexDirection: "row",
+          justifyContent: "center",
+        }}
+      >
+        <ActivityIndicator />
+      </View>
+    );
   } else {
     return (
       <MapView
