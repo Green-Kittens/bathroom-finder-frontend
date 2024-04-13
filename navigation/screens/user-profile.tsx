51b6175ceaf48b5b61b90cf43fc72b82e47f863f
Megan Fung <emailmeganfung@gmail.com>
2024-04-13 14:06:15 -0700

    applied image picker to user-profile screen

--- navigation/screens/user-profile.tsx
+++ navigation/screens/user-profile.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Image,
   ScrollView,
@@ -6,8 +6,12 @@ import {
   Button,
   Text,
   View,
+  TouchableOpacity,
+  Modal,
+  Alert,
 } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import * as ImagePicker from "expo-image-picker";
 
 // types
 import { ScreenNavigationProp } from "../type";
@@ -16,17 +20,130 @@ export default function UserProfileScreen() {
   // navigation
   const navigation = useNavigation<ScreenNavigationProp>();
 
+  // add photo modal
+  const [modalVisible, setModalVisible] = useState(false);
+  const ImageUploader = ({
+    isVisible,
+    onClose,
+  }: {
+    isVisible: boolean;
+    onClose: () => void;
+  }) => {
+    return (
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={isVisible}
+        onRequestClose={onClose}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Button
+              title="Take Photo"
+              color="#344f33"
+              onPress={addUsingCamera}
+            />
+            <Button
+              title="Choose from Gallery"
+              color="#344f33"
+              onPress={addFromGallery}
+            />
+            <Button
+              title="Remove Current Image"
+              color="#344f33"
+              onPress={deleteImage}
+            />
+            <Button
+              title="Cancel"
+              color="red"
+              onPress={() => {
+                onClose();
+              }}
+            />
+          </View>
+        </View>
+      </Modal>
+    );
+  };
+
+  // images uploaded for review
+  const [pfp, setImage] = useState("https://upload.wikimedia.org/wikipedia/commons/b/b2/Hausziege_04.jpg");
+
+  // permissions
+  const [cameraStatus] = ImagePicker.useCameraPermissions();
+  const [galleryAccessStatus] = ImagePicker.useMediaLibraryPermissions();
+
+  // add photo from device gallery
+  const addFromGallery = async () => {
+    if (galleryAccessStatus?.status !== "granted") {
+      const { status } =
+        await ImagePicker.requestMediaLibraryPermissionsAsync();
+      if (status !== "granted") {
+        Alert.alert("gallery access permission denied");
+        return;
+      }
+    }
+    const _image = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+    if (!_image.canceled) {
+      setImage(_image.assets[0].uri);
+      setModalVisible(false);
+    }
+  };
+
+  // add photo using camera
+  const addUsingCamera = async () => {
+    if (cameraStatus?.status !== "granted") {
+      const { status } = await ImagePicker.requestCameraPermissionsAsync();
+      if (status !== "granted") {
+        Alert.alert("camera permission denied");
+        return;
+      }
+    }
+    const _image = await ImagePicker.launchCameraAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      base64: true,
+      quality: 1,
+    });
+    if (!_image.canceled) {
+      setImage(_image.assets[0].uri);
+      setModalVisible(false);
+    }
+  };
+
+  // delete uploaded image
+  const deleteImage = () => {
+    setImage("https://upload.wikimedia.org/wikipedia/commons/b/b2/Hausziege_04.jpg");
+    setModalVisible(false);
+  };
+
   return (
     <ScrollView>
-      <View style={styles.profilePictureContainer}>
+
+      <TouchableOpacity 
+        style={styles.profilePictureContainer}
+        onPress={() => {
+          setModalVisible(true);
+        }}
+      >
         <Image
           style={styles.profilePicture}
           source={{
-            uri: "https://upload.wikimedia.org/wikipedia/commons/b/b2/Hausziege_04.jpg",
+            uri: pfp,
           }}
         />
         <Text>Your Name</Text>
-      </View>
+      </TouchableOpacity>
+
+      <ImageUploader 
+        isVisible={modalVisible}
+        onClose={() => setModalVisible(false)}
+      />
+      
       {horizontalCards("Your Reviews")}
       {horizontalCards("Your Favorites")}
       <View>
@@ -96,5 +213,33 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     backgroundColor: "grey",
     marginHorizontal: 5,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modalView: {
+    backgroundColor: "white",
+    borderRadius: 10,
+    padding: 20,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  imageContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  imageLink: {
+    marginRight: 10,
+    color: "blue",
   },
 });
