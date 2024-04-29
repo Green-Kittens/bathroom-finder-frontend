717d0b58d5c2d10ccb7204ae62947c9fdf18590d
Lindsay Landstad <71728910+lindlands@users.noreply.github.com>
2024-04-29 11:59:36 -0700

    Facility Form Common UI (#91)
    Common UI applied to new facility form
    
    ---------
    
    Co-authored-by: Dylan Tompkins <dltompki@outlook.com>

--- navigation/screens/facility-form.tsx
+++ navigation/screens/facility-form.tsx
@@ -6,8 +6,9 @@ import {
   Text,
   View,
   ScrollView,
-  TouchableOpacity,
   Modal,
+  TouchableOpacity,
+  ImageBackground,
 } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import * as ImagePicker from "expo-image-picker";
@@ -15,7 +16,12 @@ import DateTimePickerModal from "react-native-modal-datetime-picker";
 import { ImageCarousel } from "../../components/Carousel";
 import { useImages } from "../../contexts/ImageContext"; // Ensure the import path is correct
 import { ScreenNavigationProp } from "../type";
-import MainButton, { CancelButton } from "../../components/Buttons";
+import MainButton, {
+  CancelButton,
+  SecondaryButton,
+} from "../../components/Buttons";
+
+const boomerangimage = { uri: "/assets/images/boomerang.png" };
 
 export default function FacilityForm() {
   const navigation = useNavigation<ScreenNavigationProp>();
@@ -68,64 +74,102 @@ export default function FacilityForm() {
   };
 
   return (
-    <ScrollView style={styles.scrollView}>
-      <View style={styles.container}>
-        <Text style={styles.title}>Add a New Facility</Text>
-        <View style={styles.timeSelect}>
-          <TouchableOpacity onPress={() => setOpenPickerVisibility(true)}>
-            <Text style={styles.timeSelectButton}>
-              {openTime ? openTime.toLocaleTimeString() : "Open Time"}
-            </Text>
-          </TouchableOpacity>
-          <DateTimePickerModal
-            isVisible={isOpenPickerVisible}
-            mode="time"
-            onConfirm={handleOpenConfirm}
-            onCancel={() => setOpenPickerVisibility(false)}
+    <View style={styles.container}>
+      <ImageBackground
+        source={boomerangimage}
+        style={{
+          width: 1070,
+          height: 1000,
+          position: "absolute",
+          top: 550,
+          left: -200,
+        }}
+        imageStyle={{
+          resizeMode: "cover",
+          alignSelf: "flex-end",
+        }}
+      ></ImageBackground>
+      <ScrollView style={{ width: "100%" }}>
+        <View
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+            alignContent: "center",
+          }}
+        >
+          <Text style={styles.title}>Add a New Facility</Text>
+          <View style={styles.timeSelect}>
+            <TouchableOpacity onPress={() => setOpenPickerVisibility(true)}>
+              <Text style={styles.timeSelectButton}>
+                {openTime
+                  ? openTime.toLocaleTimeString([], {
+                      hour: "2-digit",
+                      minute: "2-digit",
+                    })
+                  : "Open Time"}
+              </Text>
+            </TouchableOpacity>
+            <DateTimePickerModal
+              isVisible={isOpenPickerVisible}
+              mode="time"
+              onConfirm={handleOpenConfirm}
+              onCancel={() => setOpenPickerVisibility(false)}
+            />
+            <TouchableOpacity>
+              <Text style={[styles.subtext, { marginBottom: 10 }]}> to </Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => setClosedPickerVisibility(true)}>
+              <Text style={styles.timeSelectButton}>
+                {closedTime
+                  ? closedTime.toLocaleTimeString([], {
+                      hour: "2-digit",
+                      minute: "2-digit",
+                    })
+                  : "Close Time"}
+              </Text>
+            </TouchableOpacity>
+            <DateTimePickerModal
+              isVisible={isClosedPickerVisible}
+              mode="time"
+              onConfirm={handleClosedConfirm}
+              onCancel={() => setClosedPickerVisibility(false)}
+            />
+          </View>
+          <ImageCarousel componentId="facilityForm" />
+          <TextInput
+            style={styles.input}
+            placeholder="write your description..."
+            placeholderTextColor="#6da798"
+            value={description}
+            onChangeText={setDescription}
+            multiline={true}
           />
-          <Text> to </Text>
-          <TouchableOpacity onPress={() => setClosedPickerVisibility(true)}>
-            <Text style={styles.timeSelectButton}>
-              {closedTime ? closedTime.toLocaleTimeString() : "Close Time"}
-            </Text>
-          </TouchableOpacity>
-          <DateTimePickerModal
-            isVisible={isClosedPickerVisible}
-            mode="time"
-            onConfirm={handleClosedConfirm}
-            onCancel={() => setClosedPickerVisibility(false)}
-          />
-        </View>
-        <ImageCarousel componentId="facilityForm" />
-        <TextInput
-          style={styles.input}
-          placeholder="Write your description..."
-          multiline={true}
-          value={description}
-          onChangeText={setDescription}
-        />
-        {MainButton("Add Photo", () => setModalVisible(true))}
-        {modalVisible && (
-          <Modal
-            animationType="fade"
-            transparent={true}
-            visible={modalVisible}
-            onRequestClose={() => setModalVisible(false)}
-          >
-            <View style={styles.centeredView}>
-              <View style={styles.modalView}>
-                {MainButton("Take Photo", () => handleAddImage("camera"))}
-                {MainButton("Choose from Gallery", () =>
-                  handleAddImage("gallery"),
-                )}
-                {CancelButton("Close", () => setModalVisible(false))}
+
+          {MainButton("Add Photo", () => setModalVisible(true))}
+          {modalVisible && (
+            <Modal
+              animationType="fade"
+              transparent={true}
+              visible={modalVisible}
+              onRequestClose={() => setModalVisible(false)}
+            >
+              <View style={styles.centeredView}>
+                <View style={styles.modalView}>
+                  {MainButton("Take Photo", () => handleAddImage("camera"))}
+                  {MainButton("Choose from Gallery", () =>
+                    handleAddImage("gallery"),
+                  )}
+                  {CancelButton("Close", () => setModalVisible(false))}
+                </View>
               </View>
-            </View>
-          </Modal>
-        )}
-        {MainButton("Submit Facility", () => navigation.navigate("Main"))}
-      </View>
-    </ScrollView>
+            </Modal>
+          )}
+          {SecondaryButton("Submit Facility", () =>
+            navigation.navigate("Main"),
+          )}
+        </View>
+      </ScrollView>
+    </View>
   );
 }
 
@@ -133,49 +177,38 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "flex-start",
-    backgroundColor: "#afd6ae",
-    color: "#344f33",
-  },
-  scrollView: {
-    flex: 1,
-    backgroundColor: "#afd6ae",
+    justifyContent: "center",
+    backgroundColor: "#EEF8F7",
+    height: "100%",
   },
   title: {
     fontSize: 30,
     fontFamily: "EudoxusSans-Bold",
-    marginTop: 20,
-    color: "#344f33",
   },
   icon: {
     marginLeft: "auto",
   },
   subtext: {
     fontSize: 17,
-    marginTop: 20,
-    marginBottom: 20,
-    color: "#344f33",
+    color: "#6da798",
   },
   errorText: {
     fontSize: 17,
     marginBottom: 20,
-    color: "red",
+    color: "#540F00",
   },
   input: {
-    borderWidth: 1,
-    borderColor: "#344f33",
+    backgroundColor: "#CDEEEA",
     borderRadius: 20,
     padding: 10,
     width: "85%",
     minHeight: 150,
     marginBottom: 20,
-    color: "#344f33",
-    alignContent: "center",
   },
   timeSelect: {
     fontSize: 17,
     marginBottom: 20,
-    color: "#344f33",
+    color: "#6da798",
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
@@ -184,9 +217,9 @@ const styles = StyleSheet.create({
     fontSize: 17,
     marginTop: 10,
     marginBottom: 20,
-    color: "#344f33",
+    color: "#6da798",
     borderWidth: 1,
-    borderColor: "#344f33",
+    borderColor: "#6da798",
     borderRadius: 20,
     padding: 10,
   },
