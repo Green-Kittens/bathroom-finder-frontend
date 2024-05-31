6901ad7f67d1efe7308098efafa6b57cf354d52a
Dylan Tompkins <dltompki@outlook.com>
2024-05-31 12:53:55 -0700

    description text

--- navigation/screens/facility-profile.tsx
+++ navigation/screens/facility-profile.tsx
@@ -25,7 +25,11 @@ import { Review as BathroomReview } from "../../types/review";
 
 const windowHeight = Dimensions.get("window").height;
 
-function CollapseView() {
+function CollapseView(props: {
+  hours: string;
+  category: string;
+  tags: string[];
+}) {
   type onPressCallback = () => void;
   function renderViewMore(onPress: onPressCallback) {
     return renderPressableText("View more", onPress);
@@ -44,17 +48,9 @@ function CollapseView() {
       renderViewLess={renderViewLess}
     >
       <Text>
-        Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit
-        enim labore culpa sint ad nisi Lorem pariatur mollit ex esse
-        exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit
-        nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor
-        minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure
-        elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor
-        Lorem duis laboris cupidatat officia voluptate. Culpa proident
-        adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod.
-        Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim.
-        Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa
-        et culpa duis.
+        Hours: {props.hours} {"\n"}
+        Category: {props.category} {"\n"}
+        Tags: {props.tags.join(", ")}
       </Text>
     </ViewMoreText>
   );
@@ -152,7 +148,11 @@ export default function TabFacilityProfileScreen() {
           }}
         >
           <View style={{ paddingVertical: "5%" }}>
-            <CollapseView />
+            <CollapseView
+              hours={bathroom.Operations}
+              category={bathroom.Category}
+              tags={bathroom.Tags}
+            />
           </View>
 
           <View>
