7924a9114cefd59466097525adc31570f44de485
Megan Fung <emailmeganfung@gmail.com>
2024-06-03 21:38:23 -0700

    fixed marker coordinates and address

--- screens/facility-form.tsx
+++ screens/facility-form.tsx
@@ -30,6 +30,10 @@ const btnActive = "#044962";
 export default function FacilityForm() {
   const navigation = useNavigation<ScreenNavigationProp>();
   const [mapModal, setMapModal] = useState(false);
+  const [markerCoordinates, setMarkerCoordinates] = useState({
+    latitude: 0, 
+    longitude: 0,
+  });
   const [markerAddress, setMarkerAddress] = useState("");
   const [currentLocation, setCurrentLocation] =
     useState<Location.LocationObject | null>(null);
@@ -158,6 +162,23 @@ export default function FacilityForm() {
       if (status === "granted") {
         const location = await Location.getCurrentPositionAsync();
         setCurrentLocation(location);
+        setMarkerCoordinates({
+          latitude: location.coords.latitude + 0.000001,
+          longitude: location.coords.longitude + 0.000001,
+        });
+        try {
+          const response = await axios.get(
+            `https://nominatim.openstreetmap.org/reverse?lat=${markerCoordinates.latitude}&lon=${markerCoordinates.longitude}&format=json`,
+         );
+         const addressDetails = response.data.address;
+         let buildingName = "";
+         if (addressDetails && addressDetails.building) {
+          buildingName = addressDetails.building;
+         }
+         setMarkerAddress(buildingName || response.data.display_name);
+        } catch (error) {
+          console.error("Error fetching address:", error);
+        }
       }
     };
     getLocation();
@@ -208,15 +229,13 @@ export default function FacilityForm() {
               <Marker
                 draggable // enables user to drag to desired location
                 tappable // enables user to tap the marker and trigger modal
-                coordinate={{
-                  latitude: currentLocation.coords.latitude + 0.000001,
-                  longitude: currentLocation.coords.longitude + 0.000001,
-                }}
-                onPress={async (e) => {
+                coordinate={markerCoordinates}
+                onDragEnd={async (e) => {
                   const { latitude, longitude } = e.nativeEvent.coordinate;
+                  setMarkerCoordinates({ latitude, longitude });
                   try {
                     const response = await axios.get(
-                      `https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json`,
+                      `https://nominatim.openstreetmap.org/reverse?lat=${markerCoordinates.latitude}&lon=${markerCoordinates.longitude}&format=json`,
                     );
                     const addressDetails = response.data.address;
                     let buildingName = "";
@@ -226,11 +245,11 @@ export default function FacilityForm() {
                     setMarkerAddress(
                       buildingName || response.data.display_name,
                     );
-                    setMapModal(true);
                   } catch (error) {
                     console.error("Error fetching address:", error);
                   }
                 }}
+                onPress={() => setMapModal(true)}
               ></Marker>
             </MapView>
           ) : (
