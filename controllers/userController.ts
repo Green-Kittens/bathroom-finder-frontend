1d37ad461b6e709e962b77fc50ad01c13fe33d67
Jonas <j.schiessl2001@gmail.com>
2024-06-04 21:29:08 -0700

    updated controller

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -5,6 +5,7 @@ import { port, host, protocol } from "./env";
 
 /**
  * Function to register a new user
+ * @param {string} UserID - The user's ID
  * @param {string} DisplayName - The user's display name
  * @param {string} Email - The email address of the user
  * @param {string} PictureURL - Profile display image
@@ -14,6 +15,7 @@ import { port, host, protocol } from "./env";
  * @returns {Promise<string>} - Returns a promise with a success message upon successful registration
  */
 export async function registerUser(
+  UserID: string,
   Email: string,
   Favorites: string[],
   Reviews: string[],
@@ -23,12 +25,13 @@ export async function registerUser(
 ): Promise<string> {
   try {
     await axios.post<UserProfile>(`${protocol}://${host}:${port}/users/`, {
-      email: Email,
-      Favorites: Favorites,
-      Reviews: Reviews,
-      DateJoined: Date,
-      pfpURL: PictureURL,
-      DisplayName: DisplayName,
+      UserID,
+      Email,
+      Favorites,
+      Reviews,
+      Date,
+      PictureURL,
+      DisplayName,
     });
     const successsMsg = `${DisplayName} successfully created with ${Email}.`;
     return successsMsg;
@@ -55,56 +58,3 @@ export async function getUserProfile(UserID: string): Promise<UserProfile> {
   }
 }
 
-/**
- * Function for creating a bathroom
- * @param {string} Name - Name of the bathroom
- * @param {number[]} Coordinates - Location of bathroom
- * @param {string} Tags - Tags on the bathroom
- * @param {string} Operations - Bathroom's hours of operation
- * @param {string[]} Reviews - Reviews on bathroom
- * @param {Date} Date - Date the bathroom was created/posted
- * @param {string[]} PictureURL - Picture(s) of the bathroom
- * @param {number} RatingAVG - The rating average of the bathroom
- * @param {number} Favorites - The number of favorites the bathroom has
- * @param {number} Reports - The number of reports on the bathroom
- * @returns {Promise<string>} - Returns a promise with a success message upon successfully creating a new bathroom
- */
-export async function createBathroom(
-  Name: string,
-  Coordinates: [number, number],
-  Category: string,
-  Tags: string,
-  Operations: string,
-  Reviews: string[],
-  Date: Date,
-  PictureURL: string[],
-  RatingAVG: number,
-  Favorites: number,
-  Reports: number,
-): Promise<string> {
-  try {
-    await axios.post<BathroomProfile>(
-      `${protocol}://${host}:${port}/facilities/`,
-      {
-        params: {
-          Name: Name,
-          Coordinates: Coordinates,
-          Category: Category,
-          Tags: Tags,
-          Operations: Operations,
-          Reviews: Reviews,
-          Date: Date,
-          PictureURL: PictureURL,
-          RatingAVG: RatingAVG,
-          Favorites: Favorites,
-          Reports: Reports,
-        },
-      },
-    );
-    const successMsg = `Bathroom successfully created: hours of operation (${Operations}), tags (${Tags})).`;
-    return successMsg;
-  } catch (error) {
-    console.error("Error creating bathroom:", error);
-    throw error;
-  }
-}
