cf512f70037c84c15f335150c3e84fbe5703b23d
Dylan Tompkins <dltompki@outlook.com>
2024-06-04 22:38:42 -0700

    disable eslint on line

--- controllers/imagesController.ts
+++ controllers/imagesController.ts
@@ -15,7 +15,8 @@ export async function uploadImage(image: ImagePickerAsset): Promise<string> {
       name: image.fileName || "photo.jpg",
       type: image.type || "image/jpg",
       uri: Platform.OS === "ios" ? image.uri.replace("file://", "") : image.uri,
-    } as any);
+    } as any); // eslint-disable-line
+    // have to disable eslint above because of the cast to any but I couldn't get it to work otherwise
 
     const res = await axios.put(`${protocol}://${host}:${port}/images/`, form, {
       headers: {
